80f2769ffafa6a74f9942aad0d1c4adf2944ee8e
Djeison Girardi Farias <32227606+djeisonfarias@users.noreply.github.com>
2024-09-08 00:36:49 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,37 @@
+
+// let utilizado para declarar variável
+// let datos = "dados"; (Declaração de variável única)
+// Abaixo a declaração de uma variável de lista para armazenar mais informações
+// O JavaScript aceita aspas duplas ou simples para a declaração da string na variável.
+// A enumeração da lista inicia com o índice 0
+
+//let dados = ["Rayssa Leal", "Rebeca Andrade", "Beatriz Souza"]; 
+
+//podemos criar um objeto (dados) ao invés de criar uma lista de listas devido ao alto volume
+//de conteúdo:
+
+let dados = [
+  {
+    titulo: "Rayssa Leal",
+    descricao: "Rayssa Leal, a Fadinha, é um dos maiores nomes do skate mundial.",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "sk8 skatista ollie",
+    foto : "rayssa-leal.jpeg"
+
+        
+  },
+  {
+   titulo: "Beatriz Souza",
+   descricao: "Beatriz Souza é uma destacada judoca brasileira.", // Substitua por uma descrição mais completa
+   link: "https://pt.wikipedia.org/wiki/Beatriz_Souza", 
+   tags: "judoca luta faixa" ,
+   foto : "beatriz-souza.jpeg"   
+  },
+  {
+   titulo: "Rebeca Andrade",
+   descricao: "Rebeca Andrade é uma ginasta brasileira, medalhista olímpica.", // Substitua por uma descrição mais completa
+   link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",    
+   tags: "ginástica solo cavalo",
+   foto : "rebeca-andrade.jpg"
+  }
+  ];
